8e828cad1cce38c28a4badd2c5c32e3a8f78134c
yiwaiyc <yiwaiyc@163.com>
2021-03-08 18:16:24 +0800

    npm依赖打包脚本文件

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+import re
+import shutil
+print("ceshi..")
+
+# for root, ds, fs in os.walk("E:/study/Code/npm/node_modules"):
+#     for f in fs:
+#         if (f == "package.json"):
+#             os.chdir(root)
+#             os.system("npm pack")
+for root, ds, fs in os.walk("E:/study/Code/npm/node_modules"):
+    for f in fs:
+        if re.match(r'.*tgz$', f):
+            name, suffix = f.rsplit(".tgz")
+            print(root+"\\"+f)
+            print("E:/study/Code/npm/new"+name+".tgz")
+            shutil.copyfile(root+"\\"+f, "E:/study/Code/npm/new/"+name+".tgz")
