6a76dea87b51f2c6c4581223aad5fbc7e257c2d3
yiwaiyc <yiwaiyc@163.com>
2021-03-08 18:18:46 +0800

    测试冲突

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import re
 import shutil
 print("ceshi..")
 
-# for root, ds, fs in os.walk("E:/study/Code/npm/node_modules"):
-#     for f in fs:
-#         if (f == "package.json"):
-#             os.chdir(root)
-#             os.system("npm pack")
+for root, ds, fs in os.walk("E:/study/Code/npm/node_modules"):
+    for f in fs:
+        if (f == "package.json"):
+            os.chdir(root)
+            os.system("npm pack")
 for root, ds, fs in os.walk("E:/study/Code/npm/node_modules"):
     for f in fs:
         if re.match(r'.*tgz$', f):
